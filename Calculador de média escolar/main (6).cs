fdd5c14e19e117b615715a12a263b1425243a078
Pedro Sales <113061171+psalesz@users.noreply.github.com>
2022-09-20 19:36:39 -0300

    Add files via upload

---
+++ Calculador de média escolar/main (6).cs
@@ -0,0 +1,72 @@
+using System;
+
+public class HelloWorld
+{
+    public static void Main(string[] args)
+    {
+        Console.WriteLine ("Digite o nome do aluno:");
+        string nome;
+        nome = Console.ReadLine();
+        
+        Console.WriteLine ("Digite a primeeira nota do aluno:");
+        double nota1;
+        nota1 = double.Parse(Console.ReadLine());
+        
+        Console.WriteLine ("Digite a segunda nota do aluno:");
+        double nota2;
+        nota2 = double.Parse(Console.ReadLine());
+        
+        Console.WriteLine ("Digite a terceira nota do aluno:");
+        double nota3;
+        nota3 = double.Parse(Console.ReadLine());
+        
+        Console.WriteLine ("Digite a última nota do aluno:");
+        double nota4;
+        nota4 = double.Parse(Console.ReadLine());
+        
+        double media;
+        media = (nota1 + nota2 + nota3 + nota4) / 4;
+        
+        Console.WriteLine("A MÉDIA FINAL DO ALUNO É DE:" + media);
+        
+        if (media < 6.0)  {
+            Console.WriteLine("ESTE ALUNO FOI REPROVADO");
+        }
+        else if (media > 6.0) {
+            Console.WriteLine("ESTE ALUNO FOI APROVADO");
+        }
+        Console.WriteLine("Deseja calcular a média de outro aluno digite 'x' ");
+        char repetir;
+        repetir = char.Parse(Console.ReadLine());
+        
+        while (repetir == 'x') {
+            Console.WriteLine ("Digite o nome do aluno:");
+            nome = Console.ReadLine();
+            
+            Console.WriteLine ("Digite a primeeira nota do aluno:");
+            nota1 = double.Parse(Console.ReadLine());
+            
+            Console.WriteLine ("Digite a segunda nota do aluno:");
+            nota2 = double.Parse(Console.ReadLine());
+            
+            Console.WriteLine ("Digite a terceira nota do aluno:");
+            nota3 = double.Parse(Console.ReadLine());
+            
+            Console.WriteLine ("Digite a última nota do aluno:");
+            nota4 = double.Parse(Console.ReadLine());
+            
+            media = (nota1 + nota2 + nota3 + nota4) / 4;
+            
+            Console.WriteLine("A MÉDIA FINAL DO ALUNO É DE:" + media);
+            
+            if (media < 6.0)  {
+            Console.WriteLine("ESTE ALUNO FOI REPROVADO");
+        }
+        else if (media > 6.0) {
+            Console.WriteLine("ESTE ALUNO FOI APROVADO");
+        }
+        Console.WriteLine("Deseja calcular a média de outro aluno digite 'x' ");
+        repetir = char.Parse(Console.ReadLine());
+        }
+    }
+}
